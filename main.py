879cd8575c9aafdab48bc16e91e7c8ceff2bee35
nathandoesmuch <47115079+nathandoesmuch@users.noreply.github.com>
2020-02-07 14:37:49 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There, daddy!</h1>"
+@app.route('')
+def index():
+    return redirect("https://www.linkedin.com/in/nathandoesmuch/", code=302)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
