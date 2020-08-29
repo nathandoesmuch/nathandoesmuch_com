e57a0f5ab02a6a7c6e7126cbac0e3a5f93bd5fab
nathandoesmuch <47115079+nathandoesmuch@users.noreply.github.com>
2020-08-29 14:27:08 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 # coding=utf-8
-from flask import Flask, redirect, render_template
-import random
+from flask import Flask, render_template
 app = Flask(__name__)
-
-list_of_quotes = ['If at first you don\'t succeed... so much for skydiving.',
-                  'I tell you what always catches my eye. Short people with an umbrella.',
-                  'Wouldn\'t exercise be more fun if calories screamed while you burned them?']
 
 @app.route('/')
 def index():
-    random.shuffle(list_of_quotes)
-    return render_template('index.html', list_of_quotes=list_of_quotes)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
